ccfe5f8fb9903f968443bbae4cf46e78174db376
Om Nigam <devomini18@gmail.com>
2026-05-29 01:56:26 +0530

    feat: add favicon images and site manifest for improved branding

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -22,6 +22,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_io/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <main className="relative z-10">
           <Header />
