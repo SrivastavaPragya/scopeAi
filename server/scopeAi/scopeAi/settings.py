4530ee7f8d0105a7cf1ebcaa682b7b00dfb8df72
Om Nigam <devomini18@gmail.com>
2026-05-28 20:51:05 +0530

    fix: update ALLOWED_HOSTS assignment to use env_list for better configuration

--- server/scopeAi/scopeAi/settings.py
+++ server/scopeAi/scopeAi/settings.py
@@ -22,8 +22,7 @@ if not SECRET_KEY:
     raise ImproperlyConfigured("DJANGO_SECRET_KEY is required.")
 
 DEBUG = env_bool("DJANGO_DEBUG")
-# ALLOWED_HOSTS = env_list("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1")
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env_list("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1")
 CSRF_TRUSTED_ORIGINS = env_list("CSRF_TRUSTED_ORIGINS")
 CORS_ALLOWED_ORIGINS = env_list(
     "CORS_ALLOWED_ORIGINS",
